2fb0703c93e324c69be399a827f2d7ee4d81a0ec
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-05-11 23:00:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,9 @@ resource "aws_s3_bucket" "this" {
 terraform {
 
   backend "s3" {
-    bucket         = "manuchandrasekhar-tfstate"
+    bucket         = "digger-tfstate"
     key            = "test"
     region         = "us-east-1"
-    dynamodb_table = "tfstate"
   }
 required_providers {
     aws = {
